09e459b6314e99f292b47c3224800324f761f18b
sunwangme <sunwangme@gmail.com>
2016-06-07 21:34:43 +0800

    update

--- main.go
+++ main.go
@@ -1,13 +1,27 @@
 package main
 
-import "fmt"
+import "log"
 
-import _ "google.golang.org/grpc"
-import _ "github.com/sunwangme/bfgo/api/bfcta"
-import _ "github.com/sunwangme/bfgo/api/bfdatafeed"
-import _ "github.com/sunwangme/bfgo/api/bfgateway"
-import _ "github.com/sunwangme/bfgo/api/bfkv"
+import "golang.org/x/net/context"
+import "google.golang.org/grpc"
+import . "github.com/sunwangme/bfgo/api/bfgateway"
+
+const (
+	address = "localhost:50051"
+	message = "bfgo"
+)
 
 func main() {
-	fmt.Println("bfgo")
+	conn, err := grpc.Dial(address, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := NewBfGatewayServiceClient(conn)
+
+	r, err := c.Ping(context.Background(), &BfPingData{Message: message})
+	if err != nil {
+		log.Fatalf("could not Ping: %v", err)
+	}
+	log.Printf("ctpgateway Pong: %s", r.Message)
 }
